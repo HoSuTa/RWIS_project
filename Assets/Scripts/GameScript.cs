b87d82405ee142cdae832426638731ebfc0abb91
shumpei.sugita <sugita.shumpei@fj.ics.keio.ac.jp>
2021-12-13 16:59:52 +0900

    fix

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -1,22 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+using Mapbox.Unity.Utilities;
 
 public class GameScript : MonoBehaviour
 {
+    [SerializeField]
+    private double [] initialLonLats;
+    [SerializeField]
+    private int       initialZoom;
+    [SerializeField]
+    private List<Vector2>   lonLatsVertices;
     // Start is called before the first frame update
     void Start()
     {
+        var abstractMap          = GetComponent<AbstractMap>();
+        abstractMap.Initialize(new Vector2d(initialLonLats[0],initialLonLats[1]),initialZoom);
+
         var lineRenderer         = GetComponent< WorldLineRenderer>();
+        lineRenderer.lonLatsVertices = lonLatsVertices;
         var areaCalculator       = GetComponent<LineAreaCalculator>();
-        areaCalculator.vertices  = lineRenderer.lonLatsVertices;
+        areaCalculator.vertices  = lonLatsVertices;
         var closedLine           = GetComponent<ClosedLine>();
-        closedLine.vertices      = lineRenderer.lonLatsVertices;
+        closedLine.vertices      = lonLatsVertices;
     }
 
     // Update is called once per frame
     void Update()
     {
+        GameObject canvas = GameObject.Find("Canvas").gameObject;
+        if (canvas!=null){
+            Debug.Log("Find!");
+        }
         var indices = GetComponent<ClosedLine>().GetClosedIndices();
         if (indices!=null&& indices.Count >= 3){
             GetComponent<LineAreaCalculator>().indices = indices;
