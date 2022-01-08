9bc38fda6e418918feb0e7987e7f7b0ec845fcd7
kengo-honda <kengohonda@keio.jp>
2022-01-08 12:31:25 +0900

    微修正

--- Assets/Scripts/InitializeMapWithRwis.cs
+++ Assets/Scripts/InitializeMapWithRwis.cs
@@ -8,6 +8,7 @@
     using UnityEngine;
     using UnityEngine.UI;
     using Mapbox.Utils;
+    using System.Drawing;
 
     public class InitializeMapWithRwis : MonoBehaviour
     {
@@ -32,6 +33,7 @@
         [SerializeField]
         private List<Vector3> list;
 
+        [SerializeField] private float lineWidth = 2.0f;
 
         private LineRenderer lineRend;
         private int positionCount;
@@ -77,6 +79,8 @@
 
             lineRend = gameObject.AddComponent<LineRenderer>();
             lineRend.useWorldSpace = false;
+            lineRend.SetWidth(lineWidth, lineWidth);
+
             positionCount = 0;
 
             // lRend.SetVertexCount(2);
@@ -84,6 +88,8 @@
 
             StartCoroutine(Map_Location());
             StartCoroutine(Save_Location());
+
+          
         }
 
         protected IEnumerator Save_Location()
@@ -126,9 +132,7 @@
 
                     _map.Initialize(LL, _map.AbsoluteZoom);
 
-                    //_map.Initialize(new Vector2d(lonLatGetter.Latitude, lonLatGetter.Longitude), _map.AbsoluteZoom);
-                    // LL_3 =_map.GeoToWorldPosition(LL);
-
+           
                     break;
                 }
                 yield return new WaitForSeconds(1.0f);
