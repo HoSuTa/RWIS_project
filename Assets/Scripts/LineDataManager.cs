36fd59eeff6f048586f3564d1d9fd4f57c02eb9e
shumpei.sugita <sugita.shumpei@fj.ics.keio.ac.jp>
2022-01-16 15:21:02 +0900

    Add (Poly)LineDataManager

---
+++ Assets/Scripts/LineDataManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDataManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private string     _userName;
+    [SerializeField]
+    private int        _areaId;
+    [SerializeField]
+    private Color      _color;
+    private GameObject _lineObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PolyLineDataManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PolyLineDataManager : MonoBehaviour
+{
+    [SerializeField]
+    private string     _userName;
+    [SerializeField]
+    private int        _areaId;
+    [SerializeField]
+    private Color      _color;
+    [SerializeField]
+    private float      _score;
+    private GameObject _lineObject;
+    private GameObject _textObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
