82da42680e22e5c83627b111d66033f9f6676e36
k0TAR <kazuki.tada@fj.ics.keio.ac.jp>
2022-01-17 20:06:14 +0900

    ScoreDataManager.

--- Assets/Scripts/Utilities/ScoreDataManager.cs
+++ Assets/Scripts/Utilities/ScoreDataManager.cs
@@ -6,18 +6,18 @@ using GssDbManageWrapper;
 [RequireComponent(typeof(UserDataManager))]
 public class ScoreDataManager : MonoBehaviour
 {
-    UserDataManager     _userDataManager;
+    UserDataManager _userDataManager;
     PolyLineDataManager _polyLineDataManager;
-    private Transform   _textBGTransform;
-    Dictionary<string, GameObject>  _scoreObjects;
+    private Transform _textBGTransform;
+    Dictionary<string, GameObject> _scoreObjects;
     // Start is called before the first frame update
     void Start()
     {
-        if (_userDataManager==null)
+        if (_userDataManager == null)
         {
             _userDataManager = GetComponent<UserDataManager>();
         }
-        if (_polyLineDataManager==null)
+        if (_polyLineDataManager == null)
         {
             _polyLineDataManager = GetComponent<PolyLineDataManager>();
         }
@@ -31,45 +31,45 @@ public class ScoreDataManager : MonoBehaviour
     void Update()
     {
         var userNameSet = new HashSet<string>();
-        foreach(var userName in _userDataManager._userNames)
+        foreach (var userName in _userDataManager._userNames)
         {
             userNameSet.Add(userName);
         }
         var removeUserNames = new List<string>();
         {
-            foreach(var userName in _scoreObjects.Keys)
+            foreach (var userName in _scoreObjects.Keys)
             {
                 if (!userNameSet.Contains(userName))
                 {
                     removeUserNames.Add(userName);
                 }
             }
-            foreach(var userName in removeUserNames)
+            foreach (var userName in removeUserNames)
             {
                 var scoreObject = _scoreObjects[userName];
                 _scoreObjects.Remove(userName);
                 Destroy(scoreObject);
             }
         }
-        foreach(var userName in _userDataManager._userNames)
+        foreach (var userName in _userDataManager._userNames)
         {
             if (!_scoreObjects.ContainsKey(userName))
             {
-                var userData    = _userDataManager.GetUserData(userName);
+                var userData = _userDataManager.GetUserData(userName);
                 var scoreObject = new GameObject();
                 scoreObject.transform.SetParent(_textBGTransform);
                 scoreObject.transform.name = "scoreBoard " + userName;
-                scoreObject.transform.localScale    = new Vector3(1, 1, 1);
+                scoreObject.transform.localScale = new Vector3(1, 1, 1);
                 scoreObject.transform.localPosition = new Vector3(0, 0, 0);
-                var scoreText   = scoreObject.AddComponent<Text>();
-                scoreText.text  = userName + ": 0";
-                scoreText.rectTransform.position  = Vector3.zero;
-                scoreText.rectTransform.offsetMin = new Vector2 (0,0);
-                scoreText.rectTransform.offsetMax = new Vector2 (0,0);
-                scoreText.rectTransform.anchorMin = new Vector3(0.01f,0.01f,0.01f);
-                scoreText.rectTransform.anchorMax = new Vector3(0.99f,0.99f,0.99f);
-                scoreText.font  = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
-                scoreText.fontSize = 20;
+                var scoreText = scoreObject.AddComponent<Text>();
+                scoreText.text = userName + ": 0";
+                scoreText.rectTransform.position = Vector3.zero;
+                scoreText.rectTransform.offsetMin = new Vector2(0, 0);
+                scoreText.rectTransform.offsetMax = new Vector2(0, 0);
+                scoreText.rectTransform.anchorMin = new Vector3(0.01f, 0.01f, 0.01f);
+                scoreText.rectTransform.anchorMax = new Vector3(0.99f, 0.99f, 0.99f);
+                scoreText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+                scoreText.fontSize = 60;
                 scoreText.color = userData._color;
                 _scoreObjects.Add(userName, scoreObject);
             }
@@ -77,19 +77,19 @@ public class ScoreDataManager : MonoBehaviour
         {
             var allPolygonPositions = _polyLineDataManager.GetAllPolygonPositions();
             var offset = 0.0f;
-            foreach(var item in _scoreObjects)
+            foreach (var item in _scoreObjects)
             {
                 var score = 0.0f;
                 if (allPolygonPositions.ContainsKey(item.Key))
                 {
-                    foreach(var (areaId,polygon) in allPolygonPositions[item.Key])
+                    foreach (var (areaId, polygon) in allPolygonPositions[item.Key])
                     {
                         score += Mathf.Abs(CalcSignedArea(polygon));
                     }
                 }
                 item.Value.GetComponent<Text>().text = item.Key + ": " + score.ToString();
                 item.Value.GetComponent<RectTransform>().offsetMax = new Vector2(0, -offset);
-                offset += 20.0f;
+                offset += 60.0f;
             }
         }
     }
